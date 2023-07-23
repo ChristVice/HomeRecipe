16408b3d9ac381c99cb4cf7d707026068668e871
Christ Vice <cristianvicencio176@hotmail.com>
2023-07-22 23:18:14 -0500

    finished right side of the home page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "../styling/HomePage.css";
 import LoginIn from "./LoginIn";
-import SignUp from "./SignUp";
+// import SignUp from "./SignUp";
+//import mainBkg from "../images/chad-montano-MqT0asuoIcU-unsplash.jpg";
 
 function HomePage() {
   return (
     <div className="landing-page">
-      <div className="login-section">
+      <div className="left-section">
         <LoginIn />
       </div>
-      <br />
-
-      <SignUp />
+      <div className="right-section">
+        <div className="main-image">
+          <div className="image-overlay"></div>
+          <div className="text-overlay">
+            <h1 className="large-pic-text">SIMPLE AND TASTY RECIPES</h1>
+            <p className="small-pic-text">
+              A recipe is soulless. The essence of the recipe must come from
+              you, the cook.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
